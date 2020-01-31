a96ad3a2abf8fac8fb99733eaa0158872784cbbb
Yanters <43695512+Yanters@users.noreply.github.com>
2020-01-31 19:08:22 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -113,6 +113,48 @@ bot.on('message', message=>{
 });
 
  
-
+function image(message,search){
+ 
+    var options = {
+        url: "http://results.dogpile.com/serp?qc=images&q=" + search,
+        method: "GET",
+        headers: {
+            "Accept": "text/html",
+            "User-Agent": "Chrome"
+        }
+    };
+ 
+ 
+ 
+ 
+ 
+    request(options, function(error, response, responseBody) {
+        if (error) {
+            return;
+        }
+ 
+ 
+        $ = cheerio.load(responseBody);
+ 
+ 
+        var links = $(".image a.link");
+ 
+        var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"));
+       
+        console.log(urls);
+ 
+        if (!urls.length) {
+           
+            return;
+        }
+ 
+        // Send result
+        message.channel.send( urls[Math.floor(Math.random() * urls.length)]);
+    });
+ 
+ 
+ 
+ 
+}
 
 bot.login(process.env.token);
