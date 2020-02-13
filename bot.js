d58c3f15f94d31fe10ce44a0db8b3644831f27ce
Yanters <43695512+Yanters@users.noreply.github.com>
2020-02-13 22:25:25 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const mysql = require("mysql");
 
 
 var con = mysql.createConnection({
-    host: "https://www.mkwk019.cba.pl/mysql/",
+    host: "https://www.mkwk019.cba.pl/mysql/index.php?token=e20cb33a50b88d78a28885d12222b279#",
     user: "konradoslaw",
     password: "zaq1@WSX",
     database: "konradowo",
